e03d6876c95308b4ad26566fde618c85d293bd77
minh <quangminh8699@gmail.com>
2025-10-22 23:43:18 +0700

    push bai-tap-2

---
+++ Java Zero To Hero/chuong-1/bai-tap-2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Cấu trúc khai báo: <kiểu_dữ_liệu> <tên_biến> = <giá_trị>;
+        int hocSinh = 24;
+        System.out.println("so luong hoc sinh " + hocSinh);
+        char diemHocKy = 'A';
+        System.out.println("diem hoc ky " + diemHocKy);
+
+        String tenlop = "12A1";
+        System.out.println("ten lop " + tenlop);
+
+        int a = 12;
+        int b = a;
+
+        String s1 = "ABC";
+        String s2 = s1;
+
+        System.out.println("b: " + b);
+        System.out.println("s2: " + s2);
+    }
+}
