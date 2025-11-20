594ba90d4d56832e7705eccef060042cc2a771b4
minh <quangminh8699@gmail.com>
2025-11-20 23:06:56 +0700

    add code

--- Thread Va Multithreading Trong Java/src/Main.java
+++ Thread Va Multithreading Trong Java/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyThread t1 = new MyThread("Thread 1");
+        MyThread t1 = new MyThread("MyThread 1");
         t1.start();
 
         System.out.println("Main thread kết thúc");
